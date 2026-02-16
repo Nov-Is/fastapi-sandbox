a7ffdab644754103ef0cb0ce94685ef9a49e6fa3
Nov-Is <jza80.na2@gmail.com>
2026-02-17 00:32:04 +0900

    query-params

--- app/main.py
+++ app/main.py
@@ -1,52 +1,17 @@
-from enum import Enum
-
 from fastapi import FastAPI
-
-class ModelName(str, Enum):
-  alexnet = "alexnet"
-  resnet = "resnet"
-  lenet = "lenet"
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
-@app.get("/")
-async def root():
-    return {"message": "Hello, FastAPI!"}
+# クエリパラメータにデフォルト値設定
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
 
-# パスパラメータの型が宣言できる
-# バリデーションもできるらしい、素晴らしい
+# オプショナルパラメータ
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-  return {"item_id": item_id}
-
-# おそらく具体的なパスを先に書けということかな、変数は後で書くべし
-@app.get("/users/me")
-async def read_user_me():
-  return {"user_id": "the current user"}
-
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-  return {"user_id: user_id"}
-
-# 再定義不可
-@app.get("/users")
-async def read_users():
-    return ["Rick", "Morty"]
-
-@app.get("/users")
-async def read_users2():
-    return ["Bean", "Elfo"]
-
-# enumでの指定 import, class定義を忘れずに
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-  # isはオブジェクトそのものかを判定
-  if model_name is ModelName.alexnet:
-    return {"model_name": model_name, "message": "Deep Learning FTW!"}
-  #  ==は値が同じか
-  if model_name.value == "lenet":
-    return {"model_name": model_name, "message": "LeCNN all the images"}
-
-  return {"model_name": model_name, "message": "Have some residuals"}
-
+async def read_item(item_id: str, q: str | None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
